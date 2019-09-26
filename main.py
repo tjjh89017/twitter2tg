868e22d1bacec17f6e0e75a4118f6c2052b32d57
tjjh89017 <tjjh89017@hotmail.com>
2019-09-26 13:03:49 +0800

    testing

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 
 from flask import Flask, request
 
+import os
 import logging
+import json
+import requests
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,11 +15,44 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+class Twitter():
+    def __init__(self):
+        self.id = None
+        self.url = 'https://twitter2tg.herokuapp.com'
+        self.bearer_token = None
+
+        r = requests.post('https://api.twitter.com/oauth2/token', auth=(os.environ['API_KEY'], os.environ['API_SECRET']))
+        if r.status_code == requests.codes.ok:
+            logger.info(r.text)
+            self.bearer_token = json.loads(r.text)['access_token']
+
+        param = {'url': self.url}
+        header = {'authorization:': 'OAuth oauth_consumer_key="{}", oauth_nonce="GENERATED", oauth_signature="GENERATED", oauth_signature_method="HMAC-SHA1", oauth_timestamp="GENERATED", oauth_token="{}", oauth_version="1.0"'.format(os.environ['API_KEY'], os.environ['ACCESS_TOKEN'])}
+        r = requests.post('https://api.twitter.com/1.1/account_activity/all/dev/webhooks.json', param=param, header=header)
+        if r.status_code == requests.codes.ok:
+            logger.info(r.text)
+            j = json.loads(r.text)
+            self.id = j.id
+
+        # check status of webhook
+        header = {'authorization:': 'Bearer {}'.format(self.bearer_token)}
+        r = requests.post('https://api.twitter.com/1.1/account_activity/all/dev/webhooks.json', param=param, header=header)
+        if r.status_code == requests.codes.ok:
+            logger.info(r.text)
+        
+        # TODO Testing
+        header = {'authorization:': 'OAuth oauth_consumer_key="{}", oauth_nonce="GENERATED", oauth_signature="GENERATED", oauth_signature_method="HMAC-SHA1", oauth_timestamp="GENERATED", oauth_token="{}", oauth_version="1.0"'.format(os.environ['API_KEY'], os.environ['ACCESS_TOKEN'])}
+        r = requests.post('https://api.twitter.com/1.1/account_activity/all/dev/webhooks/{}.json'.format(self.id), header=header)
+
+    def deinit(self):
+        pass
+
 @app.route('/')
 def index():
     return 'Hello World!'
 
 if __name__ == '__main__':
     # register webhook for twitter while startup
+    twitter = Twitter()
     # register webhook for telegram while startup
     app.run(debug=True)
