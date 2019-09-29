af9fccb7a068524348d0e1bef8bf6b68fe060122
tjjh89017 <tjjh89017@hotmail.com>
2019-09-29 15:51:13 +0800

    fix

--- subscribe-account.py
+++ subscribe-account.py
@@ -8,13 +8,12 @@ CONSUMER_SECRET = os.environ.get('API_SECRET', None)
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', None)
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET', None)
 
-#The environment name for the beta is filled below. Will need changing in future		
-ENVNAME = 'dev'
-WEBHOOK_URL = 'https://twitter2tg.herokuapp.com/webhook/twitter'
+ENVNAME = os.environ.get('ENVNAME', 'dev')
 
 twitterAPI = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-r = twitterAPI.request('account_activity/all/:%s/webhooks' % ENVNAME, {'url': WEBHOOK_URL})
+r = twitterAPI.request('account_activity/all/:%s/subscriptions' %
+                       ENVNAME, None, None, "POST")
 
+#TODO: check possible status codes and convert to nice messages
 print (r.status_code)
-print (r.text)
