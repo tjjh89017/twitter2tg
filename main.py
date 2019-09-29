528f234153c16c2b75361f9f186e9a08f0cd80e7
tjjh89017 <tjjh89017@hotmail.com>
2019-09-29 15:03:00 +0800

    fix

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-from flask import Flask, request
-
 import os
 import logging
 import json
 import requests
+import hmac
+import hashlib
+
+from http import HTTPStatus
+from flask import Flask, request
+from TwitterAPI import TwitterAPI
+
+API_KEY = os.environ.get('API_KEY', None)
+API_SECRET = os.environ.get('API_SECRET', None)
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', None)
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET', None)
+
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -17,35 +27,21 @@ app = Flask(__name__)
 
 class Twitter():
     def __init__(self):
-        self.id = None
+        self.api = None
         self.url = 'https://twitter2tg.herokuapp.com/webhook/twitter/'
-        self.bearer_token = None
 
-        r = requests.post('https://api.twitter.com/oauth2/token', auth=(os.environ['API_KEY'], os.environ['API_SECRET']))
-        if r.status_code == requests.codes.ok:
-            logger.info(r.text)
-            print(r.text)
-            self.bearer_token = json.loads(r.text)['access_token']
-
-        param = {'url': self.url}
-        header = {'authorization:': 'OAuth oauth_consumer_key="{}", oauth_nonce="GENERATED", oauth_signature="GENERATED", oauth_signature_method="HMAC-SHA1", oauth_timestamp="GENERATED", oauth_token="{}", oauth_version="1.0"'.format(os.environ['API_KEY'], os.environ['ACCESS_TOKEN'])}
-        r = requests.post('https://api.twitter.com/1.1/account_activity/all/dev/webhooks.json', param=param, header=header)
-        if r.status_code == requests.codes.ok:
-            logger.info(r.text)
-            print(r.text)
-            j = json.loads(r.text)
-            self.id = j.id
-
-        # check status of webhook
-        header = {'authorization:': 'Bearer {}'.format(self.bearer_token)}
-        r = requests.post('https://api.twitter.com/1.1/account_activity/all/dev/webhooks.json', param=param, header=header)
-        if r.status_code == requests.codes.ok:
-            logger.info(r.text)
-            print(r.text)
+        self.api = TwitterAPI(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         
-        # TODO Testing
-        header = {'authorization:': 'OAuth oauth_consumer_key="{}", oauth_nonce="GENERATED", oauth_signature="GENERATED", oauth_signature_method="HMAC-SHA1", oauth_timestamp="GENERATED", oauth_token="{}", oauth_version="1.0"'.format(os.environ['API_KEY'], os.environ['ACCESS_TOKEN'])}
-        r = requests.post('https://api.twitter.com/1.1/account_activity/all/dev/webhooks/{}.json'.format(self.id), header=header)
+        # init webhook
+        r = self.api.request('account_activity/all/:dev/webhooks', {'url': self.url})
+        logger.info("init webhook")
+        logger.info(r.status_code)
+        logger.info(r.text)
+
+        # check webhook
+        r = self.api.request('account_activity/all/webhooks')
+        logger.info(r.status_code)
+        logger.info(r.text)
 
     def deinit(self):
         pass
@@ -54,9 +50,29 @@ class Twitter():
 def index():
     return 'Hello World!'
 
-#app.route('/webhook/twitter')
-def webhook_twitter():
-    pass
+@app.route('/webhook/twitter', methods=["GET"])
+def webhook_crc():
+    crc = request.args['crc_token']
+
+    validation = hmac.new(
+        key=bytes(CONSUMER_SECRET, 'utf-8'),
+        msg=bytes(crc, 'utf-8'),
+        digestmod = hashlib.sha256
+    )
+    digested = base64.b64encode(validation.digest())
+    response = {
+        'response_token': 'sha256=' + format(str(digested)[2:-1])
+    }
+    print('responding to CRC call')
+
+    return json.dumps(response)
+
+@app.route('/webhook/twitter', methods=["POST"])
+def webhook():
+    request_json = request.get_json()
+    print(json.dumps(request_json, indent=4, sort_keys=True))
+
+    return ('', HTTPStatus.OK)
 
 if __name__ == '__main__':
     # register webhook for twitter while startup
